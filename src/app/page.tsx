ae67618cbd4e841bd0120e3d35c9955a77924a22
amro22397 <amroalmutasim22@gmail.com>
2024-12-31 10:05:20 +0400

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+'use client'
+
 import React from 'react'
+import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 
 const page = () => {
+  const router = useRouter();
   return (
-    <div>
-      
+    <div className='flex flex-row justify-center items-center my-80 gap-20'>
+      <Button variant="outline" className='text-4xl border-none shadow-md font-sans py-4 text-black/75
+      rounded-[6px]'
+      onClick={() => router.push("/en")}>English</Button>
+
+<Button variant="outline" className='text-4xl border-none shadow-md font-sans py-4 text-black/75
+      rounded-[6px]'
+      onClick={() => router.push("/ar")}>عربي</Button>
     </div>
   )
 }
